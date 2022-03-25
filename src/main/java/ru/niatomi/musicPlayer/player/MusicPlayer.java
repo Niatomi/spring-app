1317aa2915548d759decfc7d7977773fbcc2ff94
Niatomi <playervoker@gmail.com>
2022-03-25 16:33:31 +0400

    Music replaced by musicList Methods to show playing music now work with ArrayList<Music>

--- src/main/java/ru/niatomi/musicPlayer/player/MusicPlayer.java
+++ src/main/java/ru/niatomi/musicPlayer/player/MusicPlayer.java
@@ -1,16 +1,31 @@
 package ru.niatomi.musicPlayer.player;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import ru.niatomi.musicPlayer.models.Music;
+
+import java.util.List;
 
 /**
  * @author niatomi
  */
 @AllArgsConstructor
+@NoArgsConstructor
 public class MusicPlayer {
-    private Music music;
+    @Setter
+    private List<Music> musicList;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Setter
+    @Getter
+    private int volume;
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        musicList.stream().forEach(e -> System.out.println("Playing: ".concat(e.getSong())));
     }
 }
