956de71c51843c460df4d3d504ea5ab9dfba7029
Niatomi <playervoker@gmail.com>
2022-03-27 23:55:46 +0400

    Changed implementation of getSongs() with interface declaration

--- src/main/java/ru/niatomi/MusicPlayer/beans/ClassicalMusic.java
+++ src/main/java/ru/niatomi/MusicPlayer/beans/ClassicalMusic.java
@@ -2,13 +2,22 @@ package ru.niatomi.MusicPlayer.beans;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author niatomi
  */
 @Component
 public class ClassicalMusic implements Music {
+    List<String> songList = Arrays.asList(
+            "Hungary Rhapsody",
+            "Cannon in D",
+            "Four seasons");
+
     @Override
-    public String getSong() {
-        return "Hungarian Rhapsody";
+    public List<String> getSongs() {
+        return songList;
     }
 }
--- src/main/java/ru/niatomi/MusicPlayer/beans/RockMusic.java
+++ src/main/java/ru/niatomi/MusicPlayer/beans/RockMusic.java
@@ -3,14 +3,22 @@ package ru.niatomi.MusicPlayer.beans;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author niatomi
  */
 @Component
 @NoArgsConstructor
 public class RockMusic implements Music {
+    List<String> songList = Arrays.asList(
+            "It's only smiles",
+            "Marigold",
+            "Reptile");
+
     @Override
-    public String getSong() {
-        return "Wind cries Mary";
+    public List<String> getSongs() {
+        return songList;
     }
 }
