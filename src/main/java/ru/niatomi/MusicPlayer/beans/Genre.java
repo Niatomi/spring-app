a9ccae97fe392fa01fe206a105383952ef93e90f
Niatomi <playervoker@gmail.com>
2022-03-28 15:24:37 +0400

    Changed ENUM to class Genre inputs from constructor

--- src/main/java/ru/niatomi/MusicPlayer/beans/Genre.java
+++ src/main/java/ru/niatomi/MusicPlayer/beans/Genre.java
@@ -1,8 +1,20 @@
 package ru.niatomi.MusicPlayer.beans;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author niatomi
  */
-public enum Genre {
-    CLASSICAL, ROCK
+@AllArgsConstructor
+public class Genre {
+
+    @Getter
+    private String genre;
 }
