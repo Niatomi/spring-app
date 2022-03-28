9a7690916c4f7d59f082b13f50f0f7aa76082232
Niatomi <playervoker@gmail.com>
2022-03-28 15:26:12 +0400

    Changed playMusic implementation MusicPlayer constructing only from genreList

--- src/main/java/ru/niatomi/MusicPlayer/beans/MusicPlayer.java
+++ src/main/java/ru/niatomi/MusicPlayer/beans/MusicPlayer.java
@@ -1,10 +1,17 @@
 package ru.niatomi.MusicPlayer.beans;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.stereotype.Component;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static ru.niatomi.MusicPlayer.beans.Genre.*;
@@ -12,26 +19,28 @@ import static ru.niatomi.MusicPlayer.beans.Genre.*;
 /**
  * @author niatomi
  */
-@Component
-@AllArgsConstructor
 public class MusicPlayer {
 
-    @Autowired
-    @Qualifier("classicalMusic")
-    private Music classicalMusic;
+    private List<Genre> genreList = new ArrayList<>();
 
-    @Autowired
-    @Qualifier("rockMusic")
-    private Music rockMusic;
+    private ClassicalMusic classicalMusic = new ClassicalMusic();
+    private RockMusic rockMusic = new RockMusic();
 
-    public String playMusic(Enum anEnum) {
-        String returnString = "";
-        if (anEnum == CLASSICAL) {
-            returnString = "Playing: " + classicalMusic.getSongs().get(new Random().nextInt(3));
-        } else if (anEnum == ROCK) {
-            returnString = "Playing: " + rockMusic.getSongs().get(new Random().nextInt(3));
+    public MusicPlayer(List<Genre> genreList) {
+        this.genreList = genreList;
+    }
+
+    public void playMusic() {
+        Genre randomGenre = genreList.get(new Random().nextInt(genreList.size()));
+        if (randomGenre.getGenre() == "Classic") {
+            classicalMusic.getSongs()
+                          .stream()
+                          .forEach(genre -> System.out.println(genre));
+        } else if (randomGenre.getGenre() == "Rock") {
+            rockMusic.getSongs()
+                     .stream()
+                     .forEach(genre -> System.out.println(genre));
         }
-        return returnString;
     }
 
 }
