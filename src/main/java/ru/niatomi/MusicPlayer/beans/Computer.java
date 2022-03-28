0500bae46048e29631f295c89e728f561088eada
Niatomi <playervoker@gmail.com>
2022-03-28 15:26:27 +0400

    Not in use for now

--- src/main/java/ru/niatomi/MusicPlayer/beans/Computer.java
+++ src/main/java/ru/niatomi/MusicPlayer/beans/Computer.java
@@ -13,21 +13,21 @@ import static ru.niatomi.MusicPlayer.beans.Genre.*;
 @Component
 public class Computer {
 
-    private int id;
-
-    @Autowired
-    MusicPlayer musicPlayer;
-
-    public Computer(MusicPlayer musicPlayer) {
-        this.id = 1;
-        this.musicPlayer = musicPlayer;
-    }
-
-    @Override
-    public String toString() {
-        return "Computer{" +
-                "id=" + id +
-                ", musicPlayer=" + musicPlayer.playMusic(CLASSICAL) +
-                '}';
-    }
+//    private int id;
+//
+//    MusicPlayer musicPlayer;
+//
+//    @Autowired
+//    public Computer(MusicPlayer musicPlayer) {
+//        this.id = 1;
+//        this.musicPlayer = musicPlayer;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Computer{" +
+//                "id=" + id +
+//                ", musicPlayer=" + musicPlayer.playMusic(CLASSICAL) +
+//                '}';
+//    }
 }
