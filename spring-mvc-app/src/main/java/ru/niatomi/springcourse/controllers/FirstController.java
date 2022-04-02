572c5df3afcd1ba751d6747e442c9860cb42423e
Niatomi <playervoker@gmail.com>
2022-04-02 17:55:12 +0400

    Add Thymeleaf model support

--- spring-mvc-app/src/main/java/ru/niatomi/springcourse/controllers/FirstController.java
+++ spring-mvc-app/src/main/java/ru/niatomi/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.niatomi.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,8 +27,11 @@ public class FirstController {
 
     @GetMapping("/goodbye")
     public String goodbyePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                              @RequestParam(value = "surname", required = false) String surname,
+                              Model model) {
+
+        model.addAttribute("message", "Hello, " + name + " " + surname);
+
         return "first/goodbye";
     }
 
