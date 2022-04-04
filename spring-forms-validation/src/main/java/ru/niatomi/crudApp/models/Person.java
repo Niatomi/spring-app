10d78f48e20cc044f392a56656d3c0531f81f0b1
Niatomi <playervoker@gmail.com>
2022-04-05 01:54:21 +0400

    Add validation rules

--- spring-forms-validation/src/main/java/ru/niatomi/crudApp/models/Person.java
+++ spring-forms-validation/src/main/java/ru/niatomi/crudApp/models/Person.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.*;
-
 /**
  * @author niatomi
  */
@@ -15,16 +13,8 @@ import javax.validation.constraints.*;
 public class Person {
 
     private int id;
-
-    @NotEmpty(message = "Name should not be empty")
-    @Size(min = 2, max = 32, message = "Name should be between 2 and 32 chars")
     private String name;
-
-    @Min(value = 0, message = "Age should be greater than zero")
     private int age;
-
-    @NotEmpty(message = "Email should not be empty")
-    @Email(message = "Email should be valid")
     private String email;
 
 }
