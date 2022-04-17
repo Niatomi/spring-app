00c3065f9f370e9a2d917e894213b5a93ae8cefa
Niatomi <playervoker@gmail.com>
2022-04-18 00:20:16 +0400

    Initial commit

---
+++ spring-esp8266-simple-controller/src/main/java/ru/niatomi/Application.java
@@ -0,0 +1,14 @@
+package ru.niatomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author niatomi
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
