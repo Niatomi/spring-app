e0f907d140126980c3bb67a5d603373b8f2bf18c
Niatomi <playervoker@gmail.com>
2022-04-17 19:55:31 +0400

    Implemented delete, select and signUp methods

--- spring-rest/src/main/java/ru/niatomi/service/UserService.java
+++ spring-rest/src/main/java/ru/niatomi/service/UserService.java
@@ -1,6 +1,12 @@
 package ru.niatomi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.niatomi.entity.UserEntity;
+import ru.niatomi.exceptions.UserAlreadyExistsException;
+import ru.niatomi.exceptions.UserNotFoundException;
+import ru.niatomi.model.User;
+import ru.niatomi.repository.UserRepo;
 
 /**
  * @author niatomi
@@ -8,4 +14,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    @Autowired
+    private UserRepo userRepo;
+
+    public UserEntity registration(UserEntity user) throws UserAlreadyExistsException {
+        if (userRepo.findByUsername(user.getUsername()) != null) {
+            throw new UserAlreadyExistsException(user);
+        }
+        return userRepo.save(user);
+    }
+
+    public User getOneUser(Long id) throws UserNotFoundException {
+        UserEntity userEntity = userRepo.findById(id).get();
+        if (userEntity == null) {
+            throw new UserNotFoundException(id);
+        }
+        return User.toModel(userEntity);
+    }
+
+    public String deleteUser(Long id) throws UserNotFoundException {
+        userRepo.deleteById(id);
+        return "User with id=" + id + " deleted";
+    }
 }
