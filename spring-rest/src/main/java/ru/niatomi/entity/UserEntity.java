fa2c9d94cc80e68f29827365e329ed29577a172e
Niatomi <playervoker@gmail.com>
2022-04-17 19:54:34 +0400

    Add table name Add list of todo

--- spring-rest/src/main/java/ru/niatomi/entity/UserEntity.java
+++ spring-rest/src/main/java/ru/niatomi/entity/UserEntity.java
@@ -3,10 +3,8 @@ package ru.niatomi.entity;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * @author niatomi
@@ -14,11 +12,14 @@ import javax.persistence.Id;
 @Entity
 @Data
 @NoArgsConstructor
-public class User {
+@Table(name = "user_table")
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    List<TodoEntity> todos;
 
     private String username;
     private String password;
