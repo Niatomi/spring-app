4bd48bad1d4244f36c05198ace09e8a3d1ebc314
Niatomi <playervoker@gmail.com>
2022-06-12 01:28:40 +0400

    Return types changed to EntityModel

--- vaccination-start/src/main/java/com/example/vaccinationstart/controller/VaccinationController.java
+++ vaccination-start/src/main/java/com/example/vaccinationstart/controller/VaccinationController.java
@@ -4,11 +4,15 @@ import com.example.vaccinationstart.model.Person;
 import com.example.vaccinationstart.service.VaccinationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.RepresentationModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,24 +22,26 @@ public class VaccinationController {
     private final VaccinationService vaccinationService;
 
     @GetMapping("/persons")
-    public List<Person> getPersons() {
-        return vaccinationService.getPersons();
+    public RepresentationModel<CollectionModel<EntityModel<Person>>> getPersons() {
+        List<EntityModel<Person>> collect = vaccinationService.getPersons().stream().map(EntityModel::of).collect(Collectors.toList());
+        return CollectionModel.of(collect);
     }
 
     @PostMapping("/person")
-    public Person addPerson(@RequestParam String name) {
-        return vaccinationService.addPerson(name);
+    public EntityModel<Person> addPerson(@RequestParam String name) {
+        return EntityModel.of(vaccinationService.addPerson(name));
     }
 
     @PostMapping(value = "/person/{personId}/vaccination")
-    public Person scheduleVaccination(@PathVariable(name = "personId") UUID personId,
+    public EntityModel<Person> scheduleVaccination(@PathVariable(name = "personId") UUID personId,
                                                     @RequestParam(name = "date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
-        return vaccinationService.scheduleVaccination(personId, date);
+        return EntityModel.of(vaccinationService.scheduleVaccination(personId, date));
     }
 
     @GetMapping(value = "/person/{personId}/generate-certificate")
-    public ResponseMessage generateCertificate(@PathVariable UUID personId) {
-        return new ResponseMessage(vaccinationService.generateCertificate(personId));
+    public EntityModel<ResponseMessage> generateCertificate(@PathVariable UUID personId) {
+        ResponseMessage responseMessage = new ResponseMessage(vaccinationService.generateCertificate(personId));
+        return EntityModel.of(responseMessage);
     }
 
     @PostMapping("/test-data")
